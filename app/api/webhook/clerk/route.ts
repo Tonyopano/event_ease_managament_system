12a3d573e8e0f9cf4e7f0788efef682b8b46ceb9
Tonyopano <antonyopano@gmail.com>
2024-05-31 12:33:49 +0300

    Fixing clerkClient import error

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,8 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+import { WebhookEvent, clerkClient } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser, } from '@/lib/actions/user.actions'
-import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
@@ -70,7 +69,7 @@ export async function POST(req: Request) {
 
     if(newUser) {
       await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
+        publicMetadata: { 
           userId: newUser._id
         }
       })
